86c7c5565fc5ee9c417b281012190f098b3ef726
Beat Besmer <beat.besmer@gmail.com>
2017-06-27 13:24:48 +0200

    Use TableViewController for the found items

---
+++ BluetoothScanner/ItemsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ItemsViewController.swift
+//  BluetoothScanner
+//
+//  Created by Beat Besmer on 27.06.17.
+//  Copyright © 2017 Besmer Labs. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+}
